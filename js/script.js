9670196adf0ce371b1f2bbb078e44d054e5257c2
angie <noga23121993@gmail.com>
2024-08-24 18:21:33 +0300

    first task

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// save all field input in local storage...
+let levels = document.getElementById('levels');
+let lesone = document.getElementById('lesone');
+let language = document.getElementById('language');
+let date = document.getElementById('date');
+let duration = document.getElementById('duration');
+let certificate = document.getElementById('certificate');
+let textaerea = document.getElementById('Textarea');
+
+
+    localStorage.setItem('levels', levels.value);
+    localStorage.setItem('lesone', lesone.value);
+    localStorage.setItem('date', date.value);
+    localStorage.setItem('duration', duration.value);
+    localStorage.setItem('certificate', certificate.value);
+    localStorage.setItem('language', language.value);
+    localStorage.setItem('Textarea', textaerea.value);
+
